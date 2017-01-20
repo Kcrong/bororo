54583aaa809ba4dd6fd5f2db667eabcf72bea356
kcrong <kcrong@kim82536.pe.kr>
2017-01-20 14:03:17 +0900

    Add brain obj at Bot class

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+from knowledge import Brain
+
+
 class Bot:
     def __init__(self, name):
         self.name = name
+        self.brain = Brain(self.name)
 
     def __repr__(self):
         return f"<Bot {self.name}>"
