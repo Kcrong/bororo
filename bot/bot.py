87192ef8ee64445a44ab7e0deea52a79696c6eae
kcrong <kcrong@kim82536.pe.kr>
2017-01-20 15:02:40 +0900

    Add logger at Bot class

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,12 @@
 from .knowledge import Brain
+from utility.logging import Logging
 
 
 class Bot:
     def __init__(self, name):
         self.name = name
         self.brain = Brain(self.name)
+        self.logger = Logging(self.name)
 
     def __repr__(self):
         return f"<Bot {self.name}>"
