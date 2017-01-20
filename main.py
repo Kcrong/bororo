5696293e24e5971e12293f5411ca00abaa0c71b3
kcrong <kcrong@kim82536.pe.kr>
2017-01-20 17:11:46 +0900

    Add some string for debug at main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bot import Bot
 
 if __name__ == '__main__':
+    """
     b = Bot(input("Bot Name: "))
     while True:
         print(b.name, ": ", b.get_response(input("To Bot: ")))
+    """
+    b = Bot("Bororo")
+    print(b.get_response("사과는 과일이다."))
