a9ff730a258dbf337c32325b0db8640feb12a021
kcrong <kcrong@kim82536.pe.kr>
2017-01-21 16:18:54 +0900

    Add AnalysisError log, Log header

--- bot/bot.py
+++ bot/bot.py
@@ -25,25 +25,26 @@ class Bot:
         return exp
 
     def get_response(self, talk):
-        print(f"Start Analyze {talk}")
+        print(f"[Bot LOG] Start Analyze {talk}")
 
         try:
             anal = Analyser(talk)
         except AnalysisError:
             # if there is no designator
             # Just return sympathy expression
+            print(f"[Bot LOG] Can't Analysis \"{talk}\"")
             resp = Bot.make_response_sentence(sympathy)
         else:
             # else, there is designator
             known = self.brain.remember_things(anal.name)
 
             if known is None:  # if we don't know
-                print(f"Get New Thing. {anal.name}")
+                print(f"[Bot LOG] Get New Thing. {anal.name}")
                 # learn(self, name, value=None, _bool=None, force=False)
                 self.brain.learn(anal.name, anal.mean, anal.bool_type)
 
             else:  # if we already know
-                print(f"Get Already Known Thing. {anal.name}")
+                print(f"[Bot LOG] Get Already Known Thing. {anal.name}")
                 known.learn_info(anal.name, anal.mean, anal.bool_type)
 
             resp = Bot.make_response_sentence(agreement, anal.name, anal.mean)
