bd5f189f63bf69d7aa9f309dd7b61e826b4045db
kcrong <kcrong@kim82536.pe.kr>
2017-01-23 15:45:14 +0900

    Add logging brain status

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     b = Bot(input("Bot Name: "))
     while True:
         print(b.name, ": ", b.get_response(input("To Bot: ")))
+        b.brain.print_my_knowledge()
+
     """
     b = Bot("Bororo")
     print(b.get_response("사과는 과일입니다."))
