08bf1850b9412f276e85c7b7935991c840a74ad7
kcrong <kcrong@kim82536.pe.kr>
2017-01-23 15:45:25 +0900

    Add debug data at main.py

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ if __name__ == '__main__':
     """
     b = Bot("Bororo")
     print(b.get_response("사과는 과일입니다."))
+    b.brain.print_my_knowledge()
+    """
+    ------------Brain Status------------
+    Thing 사과 :
+    >>> 사과's name is 사과
+    >>> 사과's 과일 is True
+    """
+
+    print(b.get_response("사과의 색깔은 빨간색 입니다."))
+    b.brain.print_my_knowledge()
+    print(b.get_response("아이유 콘서트 가고싶다."))
+    b.brain.print_my_knowledge()
+    print(b.get_response("나는 바보다"))
+    b.brain.print_my_knowledge()
+    print(b.get_response("나는 천재다"))
+    b.brain.print_my_knowledge()
+    print(b.get_response("손건은 남자다"))
+    b.brain.print_my_knowledge()
+    print(b.get_response("손건은 여"))
