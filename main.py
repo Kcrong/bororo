c5d310481fca71b8dd1193286e8d715b5c51a2fc
kcrong <kcrong@kim82536.pe.kr>
2017-05-03 18:52:47 +0900

    Final commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 
 if __name__ == '__main__':
-    """
+
     b = Bot(input("Bot Name: "))
     while True:
         print(b.name, ": ", b.get_response(input("To Bot: ")))
@@ -12,12 +12,15 @@ if __name__ == '__main__':
     print(b.get_response("사과는 과일입니다."))
     b.brain.print_my_knowledge()
     """
+
+    """
     ------------Brain Status------------
     Thing 사과 :
     >>> 사과's name is 사과
     >>> 사과's 과일 is True
     """
 
+    """
     print(b.get_response("사과의 색깔은 빨간색 입니다."))
     b.brain.print_my_knowledge()
     print(b.get_response("아이유 콘서트 가고싶다."))
@@ -29,3 +32,4 @@ if __name__ == '__main__':
     print(b.get_response("손건은 남자다"))
     b.brain.print_my_knowledge()
     print(b.get_response("손건은 여자다."))
+    """
